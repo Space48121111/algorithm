feb92d873a60354abfb99b71f7e40a07516fe475
Stella <stellavir11@gmail.com>
2022-08-29 08:40:01 +0300

    Add files via upload

--- aug2022/symmetricTree.cpp
+++ aug2022/symmetricTree.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -12,6 +12,7 @@ struct TreeNode {
   TreeNode(int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {}
 };
 
+// recursively compare left and right vals
 class Solution {
 public:
   bool recur(TreeNode *left, TreeNode *right)
@@ -33,14 +34,87 @@ public:
   }
 };
 
+// iteratively
+// if vals are not equal, ret false, otherwise pop off from the queue
+class Solution1 {
+public:
+  bool isSymmetric(TreeNode* root)
+  {
+    queue <TreeNode*> queue;
+    if (root == NULL)
+    {
+      return true;
+    }
+    if (!root->left && !root->right)
+    {
+      return true;
+    }
+    else if (!root->left || !root->right)
+    {
+      return false;
+    }
+
+    queue.push(root);
+    queue.push(root);
+
+    TreeNode* left;
+    TreeNode* right;
+
+    while (!queue.empty())
+    {
+      if (left->val != right->val)
+      {
+        return false;
+      }
+
+      left = queue.front();
+      queue.pop();
+
+      right = queue.front();
+      queue.pop();
+
+      if (left->left && right->right)
+      {
+        queue.push(left->left);
+        queue.push(right->right);
+      }
+      else if (left->left || right->right)
+      {
+        return false;
+      }
+
+      if (left->right && right->left)
+      {
+        queue.push(left->right);
+        queue.push(right->left);
+      }
+      else if (left->right || right->left)
+      {
+        return false;
+      }
+
+    }
+    return true;
+
+   }
+  };
+
+
 int main(int argc, char*argv[])
 {
-  TreeNode *pval2 = new TreeNode(2);
-  TreeNode *pval3 = new TreeNode(2);
+  TreeNode *pval4 = new TreeNode(3);
+  TreeNode *pval5 = new TreeNode(4);
+  TreeNode *pval6 = new TreeNode(4);
+  TreeNode *pval7 = new TreeNode(3);
+  TreeNode *pval2 = new TreeNode(2, pval4, pval5);
+  TreeNode *pval3 = new TreeNode(2, pval6, pval7);
   TreeNode *pval1 = new TreeNode(1, pval2, pval3);
 
   Solution sol;
+  Solution sol1;
   bool res = sol.isSymmetric(pval1);
+  bool res1 = sol1.isSymmetric(pval1);
+
   if (res == 1)
   {
     std::cout << "True" << std::endl;
@@ -51,9 +125,16 @@ int main(int argc, char*argv[])
 
   }
 
+  cout << res1 << endl;
+  
+  delete pval4;
+  delete pval5;
+  delete pval6;
+  delete pval7;
   delete pval2;
   delete pval3;
   delete pval1;
+
 }
 
 
